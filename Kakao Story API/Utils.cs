824aa4095dc093869d356301a9082e1886ef5dc5
Howon Lee <kck4156@kagamine-rin.com>
2022-12-19 19:12:53 +0900

    버그 수정 및 UX 개선

--- Kakao Story API/Utils.cs
+++ Kakao Story API/Utils.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static StoryApi.ApiHandler.DataType;
 
 namespace StoryApi
 {
@@ -14,6 +16,116 @@ namespace StoryApi
         {
             return Convert.ToInt64((date - epoch).TotalSeconds);
         }
+
+        /// <summary>
+        /// Spaghetti code, should be refactored.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="escapeHashtag"></param>
+        /// <returns></returns>
+        public static List<QuoteData> GetQuoteDataFromString(string text, bool escapeHashtag = false)
+        {
+            text = text.Replace("\r\n", "\n");
+            text = text.Replace("\r", "\n");
+            string[] fragmentBases = text.Split(new string[] { "{!{" }, StringSplitOptions.None);
+            List<QuoteData> returnData = new List<QuoteData>();
+            int count = 0;
+            foreach (string fragmentBase in fragmentBases)
+            {
+                if (count % 2 == 0)
+                {
+                    string str = fragmentBase.Contains("}!}") ? fragmentBase.Split(new string[] { "}!}" }, StringSplitOptions.None)[1] : fragmentBase;
+                    if (str.Contains("#") && !escapeHashtag)
+                    {
+                        string[] rawStr = str.Split(new string[] { "#" }, StringSplitOptions.None);
+                        if (rawStr[0].Length > 0)
+                        {
+                            returnData.Add(new QuoteData()
+                            {
+                                type = "text",
+                                text = rawStr[0]
+                            });
+                        }
+                        for (int i = 1; i < rawStr.Length; i++)
+                        {
+                            string strNow = rawStr[i];
+                            int splitCounter = Math.Min(strNow.IndexOf(" "), strNow.IndexOf("\n"));
+                            if (splitCounter >= 0)
+                            {
+                                string hashTag = strNow.Substring(0, splitCounter);
+                                string otherStr = strNow.Substring(splitCounter);
+                                if (hashTag.Length > 0)
+                                {
+                                    returnData.Add(new QuoteData()
+                                    {
+                                        type = "hashtag",
+                                        hashtag_type = "",
+                                        hashtag_type_id = "",
+                                        text = "#" + hashTag
+                                    });
+                                }
+                                else
+                                {
+                                    returnData.Add(new QuoteData()
+                                    {
+                                        type = "text",
+                                        text = "#"
+                                    });
+                                }
+                                if (otherStr.Length > 0)
+                                {
+                                    returnData.Add(new QuoteData()
+                                    {
+                                        type = "text",
+                                        text = otherStr
+                                    });
+                                }
+                            }
+                            else
+                            {
+                                returnData.Add(new QuoteData()
+                                {
+                                    type = "hashtag",
+                                    hashtag_type = "",
+                                    hashtag_type_id = "",
+                                    text = "#" + strNow
+                                });
+                            }
+                        }
+                    }
+                    else
+                    {
+                        QuoteData quoteData = new QuoteData()
+                        {
+                            type = "text",
+                            text = str
+                        };
+                        returnData.Add(quoteData);
+                    }
+                    count++;
+                }
+                else
+                {
+                    string[] strs = fragmentBase.Split(new string[] { "}!}" }, StringSplitOptions.None);
+                    string jsonStr = strs[0];
+                    QuoteData quoteData = JsonConvert.DeserializeObject<QuoteData>(jsonStr);
+                    count++;
+                    returnData.Add(quoteData);
+                    if (strs.Length == 2)
+                    {
+                        QuoteData quoteData2 = new QuoteData()
+                        {
+                            type = "text",
+                            text = strs[1]
+                        };
+                        returnData.Add(quoteData2);
+                        count++;
+                    }
+                }
+            }
+            return returnData;
+        }
+
         public static string GetTimeString(DateTime created_at)
         {
             int offset = DateTimeOffset.Now.Offset.Hours;
